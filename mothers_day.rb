6f7d10669667617385a901f13060b9f822c41b03
ZoeCarter <zcarter-konate@micds.org>
2018-06-19 15:59:52 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,3 +1,14 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
+def mothers_day(time_of_day, name)
+ puts "Happy #{time_of_day}, #{name}!"
+end
 
+mothers_day("mothers day", "Mom")
+
+def mothers_day(time_of_day, name)
+  puts "Happy #{time_of_day}, #{name}!"
+end
+
+mothers_day("mothers day", "Beyonce")
+  
 
